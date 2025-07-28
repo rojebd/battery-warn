df36304aaeb619da4bcb9482f7f6de941fbd7ddd
rojebd <roniellberrios@gmail.com>
2025-07-28 13:20:43 -0600

    Ready. Set. Go!

---
+++ README.md
@@ -0,0 +1,9 @@
+# Battery Warn
+
+Simple rust program I run in the background to warn me when my battery is high enough or too low.
+This script just runs in the background rechecking the battery percentage every 5 minutes.
+
+If you also want to use this you need [fyi](https://codeberg.org/dnkl/fyi) to send notifications
+and also rust installed to compile it.
+
+After that you can just execute the program on startup and it just runs in a loop.
---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use std::{fs, path::PathBuf, process, thread, time};
+
+// Requires the use of fyi for sending notifications: "https://codeberg.org/dnkl/fyi"
+// Main battery you want to track
+static MAIN_BATTERY: &str = "BAT0";
+
+fn get_battery() -> Option<PathBuf> {
+    for dir in fs::read_dir("/sys/class/power_supply/").unwrap() {
+        let dir = dir.expect("[ERROR] Reading directory failed");
+        if dir.file_name() == MAIN_BATTERY {
+            let dir = dir.path().join("capacity");
+            return Some(dir);
+        }
+    }
+    None
+}
+
+fn read_battery_contents(battery: &PathBuf) -> u8 {
+    let file: String = fs::read_to_string(battery)
+        .expect("[ERROR] Can't read file to string")
+        .replace("\n", "");
+    // println!("{:?}", file.parse::<u8>().unwrap());
+    file.parse::<u8>().unwrap()
+}
+
+fn main() {
+    let battery_path = get_battery();
+    if let Some(battery_path) = battery_path {
+        loop {
+            let battery = read_battery_contents(&battery_path);
+            if battery < 20 {
+                let _ = process::Command::new("fyi")
+                    .arg("LOW BATTERY")
+                    .arg("Please plug in to power.")
+                    .status();
+            } else if battery > 85 {
+                let _ = process::Command::new("fyi")
+                    .arg("FULL BATTERY")
+                    .arg("Please unplug power.")
+                    .status();
+            }
+            // println!("{}", battery);
+            // 300 seconds = 5 minutes
+            thread::sleep(time::Duration::from_secs(300));
+        }
+    }
+}
